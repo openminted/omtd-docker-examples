eb9fab4431ca0a362f9b0da34b78bad738634c09
ilsp <galanisd@gmail.com>
2018-03-22 16:19:37 +0200

    updated.

--- run.sh
+++ run.sh
@@ -15,11 +15,15 @@ function dockerRun(){
 	echo "executor: " $command
 	# Pull & run
 	sudo docker pull $image
-	dockerRunCommand="sudo docker run  $options -v $hostIN:$contIN -v $hostOT:$contOT $image $command --input $contIN --ouput $contOT"
+	echo -e "\n\n"
+	dockerRunCommand="sudo docker run  $options -v $hostIN:$contIN -v $hostOT:$contOT $image $command --input $contIN --output $contOT"
 	echo $dockerRunCommand
+	echo -e "\n\n"
 	$dockerRunCommand
 }
 
 
-dockerRun "bibliome/uc-tdm-as-d" "alvisnlp plans/tag_xmi.plan" $(pwd)/xmiIN/ $(pwd)/xmiIN/ $(pwd)/xmiOUT/ $(pwd)/xmiOUT/ "-m 2024M"
+dockerRun "bibliome/uc-tdm-as-d" "alvisnlp plans/tag_xmi.plan" $(pwd)/xmiINWOT/ $(pwd)/xmiINWOT/ $(pwd)/uc-tdm-as-dOUT/ $(pwd)/uc-tdm-as-dOUT/ "-m 2024M"
+#dockerRun "bibliome/segmenter" "alvisnlp alvis.segmenter.plan" $(pwd)/xmiIN/ $(pwd)/xmiIN/ $(pwd)/segmenterOUT/ $(pwd)/segmenterOUT/ 
+#dockerRun "openminted/uc-ss-module-ner" "" $(pwd)/xmiIN/ $(pwd)/xmiIN/ $(pwd)/uc-ss-module-nerOUT/ $(pwd)/uc-ss-module-nerOUT/ 
 
